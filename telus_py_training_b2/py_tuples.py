1970e1e53957947cb3665c66041079f06792afc8
Balbino Aylagas <balbino.aylagas@telusinternational.com>
2021-02-04 14:21:29 -0600

    modify py tuples

--- telus_py_training_b2/py_tuples.py
+++ telus_py_training_b2/py_tuples.py
@@ -265,3 +265,38 @@ print()
 print(green)
 print(tropic)
 print(red)
+
+
+# Loop Through a Tuple
+# You can loop through the tuple items by using a for loop.
+# Iterate through the items and print the values:
+thistuple = ("apple", "banana", "cherry")
+print()
+for x in thistuple:
+    print(x)
+
+
+# Loop Through the Index Numbers
+# You can also loop through the tuple items by referring to their index number.
+# Use the range() and len() functions to create a suitable iterable.
+# Print all items by referring to their index number:
+
+thistuple = ("apple", "banana", "cherry")
+print()
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
+
+# Using a While Loop
+# You can loop through the list items by using a while loop.
+
+# Use the len() function to determine the length of the tuple, then start at 0 and loop your way through the tuple items by refering to their indexes.
+
+# Remember to increase the index by 1 after each iteration.
+# Print all items, using a while loop to go through all the index numbers:
+thistuple = ("apple", "banana", "cherry")
+i = 0
+print()
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i + 1

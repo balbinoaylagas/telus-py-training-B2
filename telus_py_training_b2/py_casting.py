d7ffcf533e78a5985b1818203c6a5118092c98f1
Balbino Aylagas <balbino.aylagas@telusinternational.com>
2021-02-03 23:13:42 -0600

    init files and py-casting

---
+++ telus_py_training_b2/py_casting.py
@@ -0,0 +1,31 @@
+"""
+python - casting
+"""
+
+# Integers:
+x = int(1)  # x will be 1
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
+
+print()
+print("*Integers*")
+print(x, y, z)
+
+# Floats:
+x = float(1)  # x will be 1.0
+y = float(2.8)  # y will be 2.8
+z = float("3")  # z will be 3.0
+w = float("4.2")  # w will be 4.2
+
+print()
+print("*Floats*")
+print(x, y, z, w)
+
+# Strings:
+x = str("s1")  # x will be 's1'
+y = str(2)  # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+print()
+print("*Strings:*")
+print(x, y, z)
